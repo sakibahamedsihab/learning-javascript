18aa9cf2ef5b206cebe4b04590d7a313fe670c45
sakibahamedsihab <sakiba505@gmail.com>
2026-02-08 19:49:42 +0600

    commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,8 +15,8 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(isLoggedIn) // 1
-console.log(booleanIsLoggedIn) // true
+// console.log(isLoggedIn) // 1
+// console.log(booleanIsLoggedIn) // true
 
 // 1 => true; 0 => false; 
 // "" => false
@@ -25,5 +25,30 @@ console.log(booleanIsLoggedIn) // true
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber) // 33
-console.log(typeof stringNumber) // string
+// console.log(stringNumber) // 33
+// console.log(typeof stringNumber) // string
+
+// *********************** Operations ***************************
+
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3) // 2 raise to the power 3
+// console.log(2%3) // 2
+
+let str1 = "hello"
+let str2 = " Shihab"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+
---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(stringNumber) // 33
+// console.log(typeof stringNumber) // string
